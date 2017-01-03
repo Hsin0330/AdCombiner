ee6bfc2438a32e00e94e72cbfd652f5daa184d42
HsinHung <sr.fuoni+github@gmail.com>
2017-01-03 18:21:48 +0800

    add mopub sdk

---
+++ app/src/main/java/com/symhung/adcombiner/adframework/AdCombineAdapter.java
@@ -0,0 +1,27 @@
+package com.symhung.adcombiner.adframework;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by HsinHung on 2017/1/3.
+ */
+
+public class AdCombineAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
